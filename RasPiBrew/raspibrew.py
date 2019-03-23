720bfa05f414d0b417f1abd79df03ba549ea61b5
Mike Sinclair <mike.sinclair@upper90brewing.com>
2019-03-23 16:48:09 -0500

    fixed path to config.xml

--- RasPiBrew/raspibrew.py
+++ RasPiBrew/raspibrew.py
@@ -456,7 +456,7 @@ if __name__ == '__main__':
     call(["modprobe", "i2c-dev"])
     
     # Retrieve root element from config.xml for parsing
-    tree = ET.parse('config.xml')
+    tree = ET.parse('/var/www/brewinator/RasPiBrew/config.xml')
     xml_root = tree.getroot()
     template_name = xml_root.find('Template').text.strip()
 
