064404828be512af08740fc2d4fbbd5600a6147a
Mike Sinclair <mike.sinclair@upper90brewing.com>
2019-03-24 13:51:01 -0500

    more ts-ing

--- RasPiBrew/Temp1Wire.py
+++ RasPiBrew/Temp1Wire.py
@@ -21,8 +21,9 @@ class Temp1Wire:
         pipe = Popen(["cat", self.oneWireDir + self.tempSensorId + "/w1_slave"],  universal_newlines=False, stdout=PIPE)
 
         result = pipe.communicate()[0].decode('utf-8').strip()
+        
         print(result)
-        print(result.decode('ascii'))
+        
         #if (result.split('\n')[0].split(' ')[11] == "YES"):
         #  temp_C = float(result.split("=")[-1])/1000 # temp in Celcius
         #else:
