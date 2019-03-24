e6875250c4e4be4fb9a81e0f137bc55472a9c71d
Mike Sinclair <mike.sinclair@upper90brewing.com>
2019-03-24 14:08:21 -0500

    a

--- RasPiBrew/raspibrew.py
+++ RasPiBrew/raspibrew.py
@@ -440,7 +440,7 @@ def tempControlProc(myTempSensor, display, pinNum, readOnly, paramStatus, status
 def logdata(tank, temp, heat):
     f = open("brewery" + str(tank) + ".csv", "ab")
     #f.write("%3.1f;%3.3f;%3.3f\n" % (getbrewtime(), temp, heat))
-    f.write("stuff", "things", "junk")
+    f.write("stuff, things, junk")
     f.close()
 
 
